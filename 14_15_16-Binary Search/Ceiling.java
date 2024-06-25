a386f42b840aec7d09d99b493d9a798acf054296
Rithin <rithinpaul10@gmail.com>
2024-06-25 14:36:07 +0530

    Floor and Ceiling Programs added

---
+++ 14_15_16-Binary Search/Ceiling.java
@@ -0,0 +1,26 @@
+public class Ceiling{
+    public static void main(String[] args){
+        int[] arr = {-7, -4, 1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
+        int target = 33;
+        int ans = findCeiling(arr, target);
+        System.out.println(ans);
+    }
+
+    static int findCeiling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(mid == target){
+                return mid;
+            }
+            else if(target < arr[mid]){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return arr[start%arr.length];
+    }
+}
